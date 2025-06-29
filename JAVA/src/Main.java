f3bc0a13dd5bea9ef044a4a63405f75a336e4440
Kishore <kishoreprogramminghub@gmail.com>
2025-06-29 21:49:20 +0530

    main file

---
+++ JAVA/src/Main.java
@@ -0,0 +1,13 @@
+import gui.JsonParserGUI;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("JSON Parser - Java Version");
+
+        // Later this can be swapped with:
+        JsonParserGUI.launch();
+
+        // For now, just a placeholder to show structure
+        System.out.println("Launching application...");
+    }
+}
